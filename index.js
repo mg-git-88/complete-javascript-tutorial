781b254ed05883459533bf424a51136c736889b9
mg-git-88 <mayukhganguly88@gmail.com>
2022-03-23 00:06:56 +0530

    use of variables

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-document.getElementById("count-el").innerText = 5
+// let myAge = 34;
+// let humanDogRatio = 7;
+// let myDogAge = myAge * humanDogRatio;
+// console.log(myDogAge);
+
+let bonusPoints = 50;
+bonusPoints += 50;
+console.log(bonusPoints);
+
+bonusPoints -= 25;
+console.log(bonusPoints);
+
+bonusPoints += 70;
+console.log(bonusPoints);
